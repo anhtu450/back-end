c974362495dc0f46839cd1c5a467179eb13d8c4d
anhtu450 <162678368+anhtu450@users.noreply.github.com>
2025-04-23 09:32:30 +0700

    Add files via upload

---
+++ TodoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAB2.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Add(string task)
+        {
+            ViewBag.Message = $"Đã thêm task: {task}";
+            return View("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            // Giả lập dữ liệu để chỉnh sửa 
+            ViewBag.Task = $"Task {id}";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, string task)
+        {
+            ViewBag.Message = $"Đã cập nhật Task {id} thành: {task}";
+            return View();
+        }
+    }
+}
